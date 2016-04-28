82a1aac4cdb6a26d02d25b6939f22522206c95ee
Peter Juras <peter.juras@mobilexag.de>
2016-04-28 15:10:04 +0200

    Fix unit tests

--- test/unit/layouts/simple.js
+++ test/unit/layouts/simple.js
@@ -22,32 +22,14 @@ describe('SimpleLayout', () => {
   it('structure', () => {
     const logger = getLogger('test');
     const layout = new SimpleLayout();
+    const error = new Error('exception');
     const message = layout.format(
-      new LogEvent('test', LogLevel.DEBUG, 'message', 'exception', logger));
+      new LogEvent('test', LogLevel.DEBUG, 'message', error, logger));
 
     const messageDate = message.substring(0, SIMPLE_LOG_FORMAT.length);
     const messageRest = message.substring(SIMPLE_LOG_FORMAT.length);
 
     expect(messageDate).to.match(simpleLogDateRegex);
-    expect(messageRest).to.equal(' - DEBUG - test - message\nexception\n');
-  });
-
-  it('exception object is logged as string', () => {
-    const logger = getLogger('test');
-    const layout = new SimpleLayout();
-
-    const exception = {
-      error: 'true',
-      errorObj: {
-        errorObjKey: 'test',
-      },
-    };
-
-    const message = layout.format(
-      new LogEvent('test', LogLevel.DEBUG, 'Exception object logging', exception, logger)
-    );
-
-    expect(message).to.not.contain('[object Object]');
-    expect(message).to.contain('{"error":"true","errorObj":{"errorObjKey":"test"}}');
+    expect(messageRest).to.equal(` - DEBUG - test - message\n${error.stack}\n`);
   });
 });
